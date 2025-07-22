fcef750407fd4a39195e261ca77d88e37fede17f
sagarmagar0709 <159641806+sagarmagar0709@users.noreply.github.com>
2025-07-22 15:14:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from sagar"
+    return "Hello From Devops Team"
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=9000)
